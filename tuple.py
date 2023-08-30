92250fc2a320c10daa92a2934aa7a67debb87e9b
JacksPro46 <jagadeeshg10@gmail.com>
2023-08-30 22:36:14 +0530

    sets and tuple added

---
+++ tuple.py
@@ -0,0 +1,7 @@
+## Tuple is also collection of values but they are immutable and denoted with () 
+
+tup_a = (24,78,91,35,89,63,52,701)
+
+print(tup_a[1])
+
+## tup_a[2] = 87 will throw an error since we can not assign new values
