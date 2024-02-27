657a438a058c5fd567b45c88451e62d04024e97b
JaganMohan Gudela (PERSISTENT SYSTEMS LIMITED) <v-jgudela@microsoft.com>
2024-02-27 22:29:43 +0530

    elif also added here

--- If_Condition.py
+++ If_Condition.py
@@ -8,8 +8,11 @@ print(data_dict["nickname"])
 
 name_list = ["Jagadeesh", "Jagan"]
 
-if data_dict["nickname"] == name_list[0]:
+if data_dict["nickname"] != name_list[0]:
     print("Both values are same")
+
+elif data_dict["name"] == name_list[1]:
+    print(data_dict["name"] +" & "+ name_list[1] + " "+"are same values")
 
 else:
     print("Both are not same") 
